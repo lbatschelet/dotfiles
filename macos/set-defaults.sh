785b45629cc4c6dcdbf252508e0628216d03d52d
Lukas <78535538+lbatschelet@users.noreply.github.com>
2025-01-01 14:03:29 +0100

    Update set-defaults.sh

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -5,6 +5,8 @@
 # The original idea (and a couple settings) were grabbed from:
 #   https://github.com/mathiasbynens/dotfiles/blob/master/.macos
 #
+# Visit https://macos-defaults.com/ for all possibilities
+#
 # Run ./set-defaults.sh and you'll be good to go.
 
 # Disable press-and-hold for keys in favor of key repeat.
@@ -14,7 +16,7 @@ defaults write -g ApplePressAndHoldEnabled -bool false
 defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1
 
 # Always open everything in Finder's list view. This is important.
-defaults write com.apple.Finder FXPreferredViewStyle Nlsv
+defaults write com.apple.finder "FXPreferredViewStyle" -string "Nlsv"
 
 # Show the ~/Library folder.
 chflags nohidden ~/Library
@@ -22,10 +24,16 @@ chflags nohidden ~/Library
 # Set a really fast key repeat.
 defaults write NSGlobalDomain KeyRepeat -int 1
 
-# Set the Finder prefs for showing a few different volumes on the Desktop.
-defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool true
-defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool true
-defaults write com.apple.finder AppleShowAllFiles -bool true
+# Finder
+defaults write com.apple.finder "ShowExternalHardDrivesOnDesktop" -bool true
+defaults write com.apple.finder "ShowRemovableMediaOnDesktop" -bool true
+defaults write com.apple.finder "AppleShowAllFiles" -bool true
+defaults write com.apple.finder "ShowPathbar" -bool true
+defaults write com.apple.finder "_FXSortFoldersFirst" -bool true
+defaults write com.apple.finder "FXDefaultSearchScope" -string "SCcf"
+defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
+defaults write com.apple.finder "FXEnableExtensionChangeWarning" -bool "false"
+defaults write NSGlobalDomain "AppleShowAllExtensions" -bool "true"
 
 # Run the screensaver if we're in the bottom-left hot corner.
 defaults write com.apple.dock wvous-bl-corner -int 5
@@ -40,3 +48,30 @@ defaults write com.apple.Safari.plist IncludeDevelopMenu -bool true
 defaults write com.apple.Safari.plist WebKitDeveloperExtrasEnabledPreferenceKey -bool true
 defaults write com.apple.Safari.plist "com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled" -bool true
 defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+
+# Dock config
+defaults write com.apple.dock "tilesize" -int "40"
+defaults write com.apple.dock "autohide" -bool "true"
+defaults write com.apple.dock "autohide-time-modifier" -float "0.2"
+defaults write com.apple.dock "autohide-delay" -float "0.1"
+defaults write com.apple.dock "show-recents" -bool "false"
+
+# Time machine
+defaults write com.apple.TimeMachine "DoNotOfferNewDisksForBackup" -bool "true"
+
+# TextEdit
+defaults write com.apple.TextEdit "RichText" -bool "false"
+
+# MissionControll
+defaults write com.apple.dock "expose-group-apps" -bool "true"
+
+# Set right click
+defaults write com.apple.driver.AppleBluetoothMultitouch.mouse "MouseButtonMode" -string "TwoButton"
+defaults write com.apple.driver.AppleHIDMouse "MouseButtonMode" -string "TwoButton"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad "TrackpadCornerSecondaryClick" -int "2"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad "TrackpadRightClick" -int "0"
+defaults write com.apple.AppleMultitouchTrackpad "TrackpadCornerSecondaryClick" -int "2"
+defaults write com.apple.AppleMultitouchTrackpad "TrackpadRightClick" -int "0"
+
+# Disable Siri in the menu bar
+defaults write com.apple.Siri StatusMenuVisible -int 0
