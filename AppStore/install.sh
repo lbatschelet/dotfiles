984ba11ab1955785fb8f914cbfcdc99ceb866ec7
Lukas <78535538+lbatschelet@users.noreply.github.com>
2025-01-01 14:29:43 +0100

    Create install.sh

---
+++ AppStore/install.sh
@@ -0,0 +1,21 @@
+# Mac App Store applications
+# AutoMute
+mas install 1118136179
+
+# mp3tag
+mas install 1532597159
+
+# tailscale
+mas install 1475387142
+
+# PastePal
+mas install 1503446680
+
+# Amphetamine
+mas install 937984704
+
+# LanguageTool
+mas install 1534275760
+
+# ForScore
+mas install 363738376
